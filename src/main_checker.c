7f04caddc01a7cffc767903d218c69fadb84efaf
PiloucK <clkuznie@student.42.fr>
2021-04-14 15:21:20 +0200

    .

--- src/main_checker.c
+++ src/main_checker.c
@@ -6,7 +6,7 @@
 /*   By: clkuznie <clkuznie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/08 15:16:34 by clkuznie          #+#    #+#             */
-/*   Updated: 2021/04/10 15:52:13 by clkuznie         ###   ########.fr       */
+/*   Updated: 2021/04/14 14:30:47 by clkuznie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,10 +54,10 @@ stack_line_print(int *stack[3], int stack_size)
 	int		i;
 
 	i = 0;
-	while (i < stack_size)
-	{
-		printf("%11d | %11d | %11d\n", stack[0][i], stack[1][i])
-	}
+	ft_printf("%11d | %11d | %11d\n\n", stack[0][i], stack[1][i], stack[2][i]);
+	while (++i <= stack_size)
+		ft_printf("%11d | %11d | %11d\n", stack[0][i] * (stack[0][0] > i), stack[1][i] * (stack[1][0] > i), stack[2][i] * (stack[2][0] > i));
+	ft_printf("\n%11s | %11s | %11s\n", "stack A", "stack B", "sorted stack");
 }
 
 void
@@ -104,6 +104,7 @@ stack_init_malloc(int *stack[3], int stack_size)
 {
 	stack[0] = NULL;
 	stack[1] = NULL;
+	stack[2] = NULL;
 	stack[0] = malloc(sizeof(int) * stack_size + 1);
 	if (stack[0])
 		stack[1] = malloc(sizeof(int) * stack_size + 1);
@@ -113,10 +114,7 @@ stack_init_malloc(int *stack[3], int stack_size)
 	{
 		stack[2] = malloc(sizeof(int) * stack_size + 1);
 		if (!stack[2])
-		{
-			free(stack[0]);
-			free(stack[1]);
-		}
+			stack_free(stack);
 	}
 	return (error_print(!stack[0] || !stack[1] || !stack[2]));
 }
@@ -130,11 +128,12 @@ main(int ac, char **av)
 	{
 		if (stack_init_malloc(stack, ac - 1))
 			return (1);
-		if (stack_init_fill(stack, ac - 1, av + 1))
+		if (stack_init_fill(stack, ac - 1, av))
 			return (1);
 		stack_line_print(stack, ac - 1);
 		// checker_params(stack, ac - 1, av + 1);
 		// checker_read();
+		stack_free(stack);
 	}
 	return (0);
 }
